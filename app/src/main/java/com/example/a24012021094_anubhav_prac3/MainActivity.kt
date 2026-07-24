c8e93b608ca35556d30cfebb53a635b63cf1b466
AnubhavKanthariya <193083347+Anubhav-0308@users.noreply.github.com>
2026-07-24 09:20:34 +0530

    Initial commit

---
+++ app/src/main/java/com/example/a24012021094_anubhav_prac3/MainActivity.kt
@@ -0,0 +1,60 @@
+package com.example.a24012021094_anubhav_prac3
+
+import android.content.Intent
+import android.net.Uri
+import android.os.Bundle
+import android.provider.AlarmClock
+import android.provider.CallLog
+import android.provider.MediaStore
+import android.widget.Button
+import android.widget.EditText
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.net.toUri
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+
+        }
+        implicitIntent()
+        explicitIntent()
+    }
+    fun implicitIntent(){
+        findViewById<Button>(R.id.btn_browse).setOnClickListener {
+            Intent(Intent.ACTION_VIEW, Uri.parse(findViewById<EditText>(R.id.editTextText3).text.toString())).also { startActivity(it) }
+        }
+        findViewById<Button>(R.id.btn_call).setOnClickListener {
+            val number=findViewById<EditText>(R.id.editTextText4).text.toString()
+            val i = Intent(Intent.ACTION_DIAL)
+            i.setData("tel:$number".toUri())
+            startActivity(i)
+        }
+        findViewById<Button>(R.id.btn_call_log).setOnClickListener {
+            Intent(Intent.ACTION_VIEW).setType(CallLog.Calls.CONTENT_TYPE).apply { startActivity(this) }
+        }
+        findViewById<Button>(R.id.btn_gallery).setOnClickListener {
+            Intent(Intent.ACTION_VIEW).setType("image/*").apply { startActivity(this) }
+        }
+        findViewById<Button>(R.id.btn_camera).setOnClickListener {
+            Intent(MediaStore.ACTION_IMAGE_CAPTURE).also { startActivity(it) }
+        }
+        findViewById<Button>(R.id.btn_alarm).setOnClickListener {
+            Intent(AlarmClock.ACTION_SHOW_ALARMS).also { startActivity(it)}
+        }
+
+    }
+    fun explicitIntent(){
+        findViewById<Button>(R.id.btn_login).setOnClickListener {
+            Intent(this@MainActivity, LoginActivity::class.java).also{startActivity(it)}
+        }
+    }
+}
